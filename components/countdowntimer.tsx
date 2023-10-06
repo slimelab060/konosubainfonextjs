16c27cd33140ff8b1345c836016480a1a4910230
SlimeLab060 <68540736+slimelab060@users.noreply.github.com>
2023-10-06 18:12:39 +0900

    0.4-C8 fix: CountDownTimer

--- components/countdowntimer.tsx
+++ components/countdowntimer.tsx
@@ -1,75 +1,55 @@
 'use client';
-import { format, differenceInSeconds } from 'date-fns';
-import { useState, useMemo, useEffect } from 'react';
-
-interface Countdown {
-  days: number;
-  hours: number;
-  minutes: number;
-  seconds: number;
-}
+import { format, formatDuration, intervalToDuration, isBefore } from 'date-fns';
+import { ja } from 'date-fns/locale';
+import { useState, useEffect } from 'react';
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from './ui/card';
 
 interface TimerProps {
   startdate: Date;
 }
 
 const Timer: React.FC<TimerProps> = ({ startdate }) => {
-  const ONE_DAY = 60 * 60 * 24;
-  const ONE_HOUR = 60 * 60;
-  const ONE_MINUTE = 60;
-  const [currentTime, setCurrentTime] = useState<number>(new Date().getTime());
-  const diffInSeconds = differenceInSeconds(startdate, currentTime);
-  console.log(format(startdate, 'yyyy-MM-dd'), format(diffInSeconds, 'yyyy-MM-dd'));
-  const getCountdown = (): Countdown => {
-    if (diffInSeconds <= 1) {
-      return {
-        days: 0,
-        hours: 0,
-        minutes: 0,
-        seconds: 0,
-      };
-    }
-    const days = Math.floor(diffInSeconds / ONE_DAY);
-    const hours = Math.floor((diffInSeconds - days * ONE_DAY) / ONE_HOUR);
-    const minutes = Math.floor((diffInSeconds - days * ONE_DAY - hours * ONE_HOUR) / ONE_MINUTE);
-    const seconds = diffInSeconds - days * ONE_DAY - hours * ONE_HOUR - minutes * ONE_MINUTE;
-    return {
-      days,
-      hours,
-      minutes,
-      seconds,
-    };
-  };
-
-  const countdown = useMemo<Countdown>(getCountdown, [ONE_DAY, ONE_HOUR, diffInSeconds]);
+  //const endOfToday = endOfDay(now);
+  const endDate = startdate;
+  const [countdown, setCountdown] = useState('');
+  const [countdownEnded, setCountdownEnded] = useState(false);
 
   useEffect(() => {
     const interval = setInterval(() => {
       const now = new Date().getTime();
-      //console.log(format(now, 'yyyy-MM-dd HH:mm:ss'));
-      setCurrentTime(now);
+      const duration = intervalToDuration({ start: now, end: endDate });
+
+      if (isBefore(endDate, now)) {
+        setCountdownEnded(true);
+        clearInterval(interval);
+      } else {
+        setCountdown(`${formatDuration(duration, { locale: ja })}`);
+      }
     }, 1000);
 
-    return () => {
-      clearInterval(interval);
-    };
-  }, []);
+    return () => clearInterval(interval);
+  }, [endDate]);
 
   return (
-    <div>
-      <p>カウントダウン</p>
-      <span>{countdown.days}日</span>
-      <span>{countdown.hours}時間</span>
-      <span>{countdown.minutes}分</span>
-      <span>{countdown.seconds}秒</span>
+    <div className="rounded-lg">
+      <Card className=" rounded-lg bg-[url('/carousel/EER.jpg')] bg-cover">
+        <CardHeader>
+          <CardTitle>カウントダウン</CardTitle>
+          <CardDescription>{endDate && <h3>期限: {format(endDate, 'yyyy-MM-dd HH:mm')}</h3>}</CardDescription>
+        </CardHeader>
+        <CardContent>
+          {!countdownEnded && <h4>残り時間: {countdown}</h4>}
+          {countdownEnded && <h4>終了</h4>}
+        </CardContent>
+      </Card>
     </div>
   );
 };
 
 export default function CountDownTimer() {
   return (
-    <div className="container text-2xl">
-      <Timer startdate={new Date(2023, 10, 10)} />
+    <div className="mt-4 grid grid-cols-1 gap-4 text-2xl">
+      <Timer startdate={new Date(2023, 9, 10, 21, 59)} />
     </div>
   );
 }
